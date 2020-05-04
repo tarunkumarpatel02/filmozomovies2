fdfeb2fa416996d7b7d39f53d09b935c2f4fff93
unknown <tarunkpatel02@gmail.com>
2020-05-04 16:22:15 +0530

    my first commit

---
+++ precache-manifest.95594aa8cc04a8a11aaa7fa0004b635f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a29438995c78c9773ded0f3be3a279",
+    "url": "/filmozomovies/index.html"
+  },
+  {
+    "revision": "58bf9c4f831b56f88a2d",
+    "url": "/filmozomovies/static/css/2.fddaa3a5.chunk.css"
+  },
+  {
+    "revision": "649b7d63e6e57cbae499",
+    "url": "/filmozomovies/static/css/main.244ffb73.chunk.css"
+  },
+  {
+    "revision": "58bf9c4f831b56f88a2d",
+    "url": "/filmozomovies/static/js/2.7413d510.chunk.js"
+  },
+  {
+    "revision": "00f89bfdd2b8ccea7cfd359d69e2f3b7",
+    "url": "/filmozomovies/static/js/2.7413d510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649b7d63e6e57cbae499",
+    "url": "/filmozomovies/static/js/main.95131305.chunk.js"
+  },
+  {
+    "revision": "0177c401b34e12f790d5",
+    "url": "/filmozomovies/static/js/runtime-main.5338f450.js"
+  },
+  {
+    "revision": "f883bd61859350cdcafa4680183745ea",
+    "url": "/filmozomovies/static/media/circle-cropped.f883bd61.png"
+  }
+]);
